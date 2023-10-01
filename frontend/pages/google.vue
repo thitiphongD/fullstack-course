d529d2a470eacc91bf376a651d4d8ccdcac23d40
PskCluB <passakon_p@hotmail.com>
2023-10-01 22:57:44 +0700

    first commit

---
+++ frontend/pages/google.vue
@@ -0,0 +1,77 @@
+<template>
+    <div class="flex flex-col h-screen p-4">
+        <div class="flex justify-end items-center gap-4">
+            <div>Gmail</div>
+            <div>ค้นรูป</div>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+                class="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round"
+                    d="M3.75 6A2.25 2.25 0 016 3.75h2.25A2.25 2.25 0 0110.5 6v2.25a2.25 2.25 0 01-2.25 2.25H6a2.25 2.25 0 01-2.25-2.25V6zM3.75 15.75A2.25 2.25 0 016 13.5h2.25a2.25 2.25 0 012.25 2.25V18a2.25 2.25 0 01-2.25 2.25H6A2.25 2.25 0 013.75 18v-2.25zM13.5 6a2.25 2.25 0 012.25-2.25H18A2.25 2.25 0 0120.25 6v2.25A2.25 2.25 0 0118 10.5h-2.25a2.25 2.25 0 01-2.25-2.25V6zM13.5 15.75a2.25 2.25 0 012.25-2.25H18a2.25 2.25 0 012.25 2.25V18A2.25 2.25 0 0118 20.25h-2.25A2.25 2.25 0 0113.5 18v-2.25z" />
+            </svg>
+            <img class="rounded-full w-12 h-12" src="~/assets/imgs/daew.png" alt="Profile" />
+        </div>
+        <div class="flex items-center justify-center h-screen">
+            <div class="flex-col">
+                <p class="text-center text-6xl font-black text-gray-900 py-6">Google</p>
+                <div class="flex justify-center items-center mb-6">
+
+                    <div class="relative flex items-center ">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                            stroke="currentColor" class="w-6 h-6 absolute left-3">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+                        </svg>
+
+
+                        <input type="text" class="border rounded-full w-96 p-3 px-10">
+                        <div class="absolute right-3 flex">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                stroke="currentColor" class="w-6 h-6">
+                                <path stroke-linecap="round" stroke-linejoin="round"
+                                    d="M2.25 8.25h19.5M2.25 9h19.5m-16.5 5.25h6m-6 2.25h3m-3.75 3h15a2.25 2.25 0 002.25-2.25V6.75A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25v10.5A2.25 2.25 0 004.5 19.5z" />
+                            </svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                stroke="currentColor" class="w-6 h-6">
+                                <path stroke-linecap="round" stroke-linejoin="round"
+                                    d="M12 18.75a6 6 0 006-6v-1.5m-6 7.5a6 6 0 01-6-6v-1.5m6 7.5v3.75m-3.75 0h7.5M12 15.75a3 3 0 01-3-3V4.5a3 3 0 116 0v8.25a3 3 0 01-3 3z" />
+                            </svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                stroke="currentColor" class="w-6 h-6">
+                                <path stroke-linecap="round" stroke-linejoin="round"
+                                    d="M6.827 6.175A2.31 2.31 0 015.186 7.23c-.38.054-.757.112-1.134.175C2.999 7.58 2.25 8.507 2.25 9.574V18a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18V9.574c0-1.067-.75-1.994-1.802-2.169a47.865 47.865 0 00-1.134-.175 2.31 2.31 0 01-1.64-1.055l-.822-1.316a2.192 2.192 0 00-1.736-1.039 48.774 48.774 0 00-5.232 0 2.192 2.192 0 00-1.736 1.039l-.821 1.316z" />
+                                <path stroke-linecap="round" stroke-linejoin="round"
+                                    d="M16.5 12.75a4.5 4.5 0 11-9 0 4.5 4.5 0 019 0zM18.75 10.5h.008v.008h-.008V10.5z" />
+                            </svg>
+                        </div>
+                    </div>
+
+
+                </div>
+                <div class="flex justify-center items-center gap-6">
+                    <button class="border w-48 p-2 rounded-md ">ค้นหาด้วย Google</button>
+                    <button class="border w-48 p-2 rounded-md ">ดีใจจังค้น แล้วเจอเลย</button>
+                </div>
+                <p class="text-center py-4">แสดง Google ใน: English</p>
+            </div>
+        </div>
+        <footer class="px-4 py-2">
+            <div class="py-4">
+                <p>ไทย</p>
+            </div>
+            <div class="flex justify-between flex-col lg:flex-row">
+                <div class="flex space-x-4 justify-center lg:justify-start">
+                    <p>เกี่ยวกับ</p>
+                    <p>โฆษณา</p>
+                    <p>ธุรกิจ</p>
+                    <p class="hidden lg:block">วิธีการทำงานของ Search</p>
+                </div>
+                <div class="flex space-x-4 justify-center lg:justify-start">
+                    <p>ความเป็นส่วนตัว</p>
+                    <p>ข้อกำหนด</p>
+                    <p>การตั้งค่า</p>
+                </div>
+            </div>
+        </footer>
+    </div>
+</template>
+  
