4f75591eb1e8d3fccae474f1681cec325b95f8a7
Thitiphong Srisavat <80618380+thitiphongD@users.noreply.github.com>
2023-10-25 14:17:18 +0700

    add login register

--- frontend/pages/login.vue
+++ frontend/pages/login.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="h-screen bg-blue-500 flex items-center justify-center">
     <Head>
-      <Title>ลงทะเบียน - Mhalong</Title>
+      <Title>เข้าสู่ระบบ - Mhalong</Title>
       <Meta name="description" content="My app description" />
     </Head>
     <form class="bg-white w-2/5 h-4/5 rounded-md p-6 novalidate">
       <div class="text-center">
-        <p class="text-2xl">Register</p>
-        <p>Sign up for using Mhalong</p>
+        <p class="text-2xl">Welcome back!</p>
+        <p>Sign in to continue using Mhalong</p>
       </div>
       <div class="mt-4 flex flex-col">
         <div class="mb-2">
@@ -43,37 +43,17 @@
           </div>
         </div>
 
-        <div class="mb-2">
-          <label for="confirmPassword">Confirm Password</label>
-          <div class="relative rounded-md">
-            <input
-              v-model="confirmPassword"
-              :type="showPasswordConfirm ? 'text' : 'password'"
-              id="confirmPassword"
-              class="border p-2 w-full rounded-md"
-              placeholder="ยืนยันรหัสผ่าน"
-              required
-            />
-            <span
-              class="absolute right-3 top-1/2 transform -translate-y-1/2 cursor-pointer"
-              @click="togglePasswordVisibility('Confirm')"
-            >
-              <Icon :name="eyeIconConfirm" color="black" />
-            </span>
-          </div>
-        </div>
-
         <div class="py-6">
           <button
             class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full"
           >
-            Sign up
+            Sign In
           </button>
         </div>
         <div class="py-2">
           <p>
-            Already have an account?
-            <a href="/login" class="text-blue-800 underline">Login</a>
+            Don’t have an account?
+            <a href="/register" class="text-blue-800 underline">Register</a>
           </p>
         </div>
       </div>
@@ -85,25 +65,17 @@
 import { ref, computed } from "vue";
 
 const password = ref("");
-const confirmPassword = ref("");
 
 const showPasswordPassword = ref(false);
-const showPasswordConfirm = ref(false);
 
 const togglePasswordVisibility = (field) => {
   if (field === "Password") {
     showPasswordPassword.value = !showPasswordPassword.value;
-  } else if (field === "Confirm") {
-    showPasswordConfirm.value = !showPasswordConfirm.value;
   }
 };
 
 const eyeIconPassword = computed(() =>
   showPasswordPassword.value ? "uil:eye" : "uil:eye-slash"
-);
-
-const eyeIconConfirm = computed(() =>
-  showPasswordConfirm.value ? "uil:eye" : "uil:eye-slash"
 );
 </script>
 
