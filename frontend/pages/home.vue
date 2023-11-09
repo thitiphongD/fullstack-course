e97a3c44880f5e103586a2cd1460a5954c8b5d51
Thitiphong Srisavat <80618380+thitiphongD@users.noreply.github.com>
2023-11-09 12:38:52 +0700

    Update home.vue

--- frontend/pages/home.vue
+++ frontend/pages/home.vue
@@ -166,13 +166,6 @@
           >
             Mhalong
           </NuxtLink>
-          <button
-            type="button"
-            @click="openVoteModal"
-            class="rounded-md bg-black/20 px-4 py-2 text-sm font-medium text-white hover:bg-black/30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75"
-          >
-            Open dialog
-          </button>
         </div>
         <div class="lg:flex lg:flex-grow items-center">
           <ul class="flex flex-col lg:flex-row list-none ml-auto">
